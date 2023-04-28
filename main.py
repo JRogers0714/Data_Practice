822ca573f4281ee7f9896d34668a5fb6c2fdf259
Joshua Rogers <Joshua.M.Rogers@Outlook.com>
2023-04-28 13:49:24 -0400

    found a solution, Idk if its the solution they are looking forr but I think it works....

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def solution(pt_one, pt_two):
-    pt_one = input("Type any string...")
-    pt_two = input("Type any string...")
-    result = False
+
+    result: bool = False
     for x in pt_one:
         for y in pt_two:
             if x == y:
                 result = True
                 return result
-
     return result
 
+    print(result)
 
-solution()
+
+print(solution(pt_one=input("Type any string..."), pt_two=input("Type any string...")))
