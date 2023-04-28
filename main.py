72b31a883a3febe041635af976ff0fcd5f8847dc
Joshua Rogers <Joshua.M.Rogers@Outlook.com>
2023-04-28 14:09:28 -0400

    commented out previous solution. am dumb. kata is looking for code that ends with.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-def solution(pt_one, pt_two):
-
-    result: bool = False
-    for x in pt_one:
-        for y in pt_two:
-            if x == y:
-                result = True
-                return result
-    return result
-
-    print(result)
 
 
-print(solution(pt_one=input("Type any string..."), pt_two=input("Type any string...")))
+
+# def solution(pt_one, pt_two):
+
+#   result: bool = False
+#    for x in pt_one:
+#    for y in pt_two:
+#         if x == y:
+#              result = True
+#               return result
+# return result
+
+# print(result)
+
+
+#  print(solution(pt_one=input("Type any string..."), pt_two=input("Type any string...")))
